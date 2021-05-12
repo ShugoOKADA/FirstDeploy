ffbbeca781e17fc17eaf5882ff5b20dfcb6b9ca1
ShugoOKADA <78585301+ShugoOKADA@users.noreply.github.com>
2021-05-12 15:03:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html jang="ja">
   <body>
-    <h1>Deploy Success!!</h1>
-    <h2>歩き食い</h2>
+    <h1>PipeLine and Deploy Success!!</h1>
     <?php phpinfo(); ?>
   </body>
 </html>
